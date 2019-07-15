851850a752fba0762c8c57c601426447c6ca849f
Kim ByungChul <bcking92@gmail.com>
2019-07-15 17:52:02 +0900

    7_15_2019

--- SWEA/python_basic_2/List_Tuple_prc_14.py
+++ SWEA/python_basic_2/List_Tuple_prc_14.py
@@ -1,6 +1,13 @@
 dicBase = (('가','깋'), ('나','닣'), ('다','딯'), ('라','맇'), ('마','밓'), ('바','빟'), ('사','싷'),('아','잏'), ('자','짛'), ('차','칳'), ('카','킿'), ('타','팋'), ('파','핗'), ('하','힣'))
 inputWord = ['막', '부모님', '비용', '비행기', '원래', '처리', '최초', '꼴', '좀', '들다', '싶다','수출', '계시다', '다', '뒤', '듣다', '함께', '아이', '무척', '보이다', '가지다', '그','자르다', '데리다', '마리', '개', '정도', '옳다', '놀이','뜨겁다']
 
+result = []
 
-result = [ y for x in range(len(dicBase)) for y in inputWord if ord(y) >= ord(dicBase[x][0]) and ord(y) <= ord(dicBase[x][1])]
+for i in range(len(dicBase)) :
+    a = []
+    for j in inputWord :
+        if ord(dicBase[i][0]) <= ord(j[0]) and ord(dicBase[i][1]) >= ord(j[0]) :
+            a.append(j)
+    result.append(a)
+            
 print(result)
