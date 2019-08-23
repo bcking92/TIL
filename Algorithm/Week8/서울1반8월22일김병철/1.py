d118a05424f6040f954c08bfbd5b5b37b0b06793
김병철 <bcking92@gmail.com>
2019-08-23 19:21:36 +0900

    subject test

--- Algorithm/Week8/서울1반8월22일김병철/1.py
+++ Algorithm/Week8/서울1반8월22일김병철/1.py
@@ -1,1 +1,9 @@
-print(bool([]))
+def box(x):
+    if x == 1:
+        return 1
+    elif  x == 2:
+        return 3
+    return box(x-1) + 2*box(x-2)
+
+for T in range(int(input())):
+    print('#{} {}'.format(T+1, box(int(input())//10)))
