d290ab7b833cd78d5cfd0751d942caf075de8d2c
Kim_ByungChul <bcking92@gmail.com>
2019-11-04 20:01:30 +0900

    vue.js

---
+++ 03_Web/JavaScript/Vue.js
@@ -0,0 +1,25 @@
+# Vue.js
+
+#### JS를 쓰는이유? Client Side Rendering을 하기 위해, SPA(Single Page Application)을 만들기 위해
+
+User들은 Browser를 이용해 웹 서비스에 접근한다. 우리가 만든 웹 서비스에서 JS를 쓰기 이전 까지는 장고 서버가 웹페이지를 렌더하는 모든 작업을 처리했고 이 방식을 Server Side Rendering이라고 한다.
+
+그런데 JS를 통해 브라우저가 웹 페이지를 렌더하게 할 수 있는데 이것을 Cilent Side Rendering이라고 한다.  Client Side Rendering을 통해 SPA(Single Page Application, 페이지를 이동하지 않고 )
+
+그렇다면 왜 SPA를 만드려고 할까?
+
+ 사용자 경험의 편리함을 위해!!!
+
+#### Reactive VS Responsive
+
+- Reactive
+
+  데이터의 변화에 반응, 자동 적용(반영)
+
+- Responsive
+
+  디바이스의 크기 변화에 반응
+
+
+
+#### HTML Directive
