98f0dc5ed1ac1ea6ded00e33517f2fb3c339d709
KimByungChul <bcking92@gmail.com>
2020-03-12 22:56:19 +0900

    linux

--- 02_Python/02_Advanced/test2.py
+++ 02_Python/02_Advanced/test2.py
@@ -1,3 +1,13 @@
-import test1
+import os
+  
+files = os.listdir('./')
+  
+for file in files:
+    if file[-2:] != 'py':
+        name = file
+        new_name = file.replace('원래 글자', '바꿀 글자')
+        name = os.path.join('./', name)
+        new_name = os.path.join('./', new_name)
+        os.rename(name, new_name)
 
-print('this is test2.py!!')
+        
